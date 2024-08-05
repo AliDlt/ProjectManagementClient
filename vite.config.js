de62de5c491f4b0f601dd50331e77fc1b48c8527
Mohammad Zamanpour <mohammadzamanpour1384@gmail.com>
2024-08-05 19:04:14 +0330

    change proxy config

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       "/api": {
         target: "https://projectmanagment.liara.run",
         changeOrigin: true,
-        secure: false,
+        secure: true,
       },
     },
   },
